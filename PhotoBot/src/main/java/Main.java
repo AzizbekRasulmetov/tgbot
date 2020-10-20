5e20c96914641661d102dd4dae1c51efeba7f6de
AzizbekRasulmetov <azizbek.rasulmetov2000@gmail.com>
2020-10-20 18:34:19 +0500

    First commit e

---
+++ PhotoBot/src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try{
+            botsApi.registerBot(new PhotoBot());
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+}
